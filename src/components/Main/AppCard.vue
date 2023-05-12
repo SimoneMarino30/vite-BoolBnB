89e27e444b102ce24768e3b601a0468dde142da2
GiuliaGlave <giulia.glave@gmail.com>
2023-05-12 16:54:20 +0200

    recover from last lost commit :(

--- src/components/Main/AppCard.vue
+++ src/components/Main/AppCard.vue
@@ -2,4 +2,4 @@
 
 <template></template>
 
-<style></style>
+<style lang="scss" scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+//todo: IMPORTA TUTTE LE ICONE CHE CI SERVONO DA FONT AWESOME -__-'
+
+createApp(App).mount("#app");
