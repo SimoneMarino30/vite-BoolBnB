afbc52ece9dab98f014b95cfd4311593f790fc2c
MichelepioBombai <michelebombai@icloud.com>
2023-05-16 01:17:12 +0200

    add apartments with api

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,57 @@
 <script>
+import axios from "axios";
 import AppHeader from "./components/Header/AppHeader.vue";
-// import footer 
+// import footer
 import AppFooter from "./components/Footer/AppFooter.vue";
-// import card 
-import AppCard from "./components/Main/AppCard.vue";
+
+import AppList from "./components/Main/AppList.vue";
 
 export default {
-
   data() {
     return {
-      title: "BoolBnB",
+      apartments: {
+        list: [],
+        pages: [],
+      },
     };
   },
-  components: { AppHeader, AppFooter, AppCard },
+  components: { AppHeader, AppFooter, AppList },
+
+  emits: ["changePage"],
+
+  methods: {
+    fetchApartments(endpoint = null) {
+      if (!endpoint) endpoint = "http://127.0.0.1:8000/api/apartments";
+
+      axios.get(endpoint).then((response) => {
+        this.apartments.list = response.data.data;
+        this.apartments.pages = response.data.links;
+      });
+    },
+  },
+
+  created() {
+    this.fetchApartments();
+  },
 };
 </script>
 
 <template>
+  <!-- Header -->
   <AppHeader />
 
   <main class="py-5">
-    <h1>{{ title }}</h1>
-
-    <AppCard />
+    <AppList
+      :apartments="apartments.list"
+      :pages="apartments.pages"
+      @changePage="fetchApartments"
+    />
 
     <router-view></router-view>
   </main>
-  
+
   <!-- footer -->
   <AppFooter />
-
 </template>
 
 <style lang="scss">
--- src/components/Main/AppCard.vue
+++ src/components/Main/AppCard.vue
@@ -1,52 +1,61 @@
-<script></script>
+<script>
+export default {
+  props: {
+    apartment: Object,
+  },
+};
+</script>
 
 <template>
-  <!-- container da togliere forse  -->
-  <div class="container">
-    <div class="row row-cols-1  row-cols-md-2 row-cols-lg-3 row-cols-xxl-4 py-5">
-
-      <!-- init card -->
-      <div class="col px-3 py-3 card-settings" v-for="n in 15">
+  <li>
+    <!-- init card -->
+    <div>
+      <div class="px-3 py-3 card-settings">
         <a href="#" class="text-reset">
           <!-- card container  -->
           <div class="card-back py-3 px-3 rounded-4">
-  
             <!-- image card  -->
             <div class="card-image-size">
-              <img src="../../assets/images/house-10b.jpg" class="img-fluid rounded-4" alt="#">
+              <img
+                src="../../assets/images/house-10b.jpg"
+                class="img-fluid rounded-4"
+                alt="#"
+              />
             </div>
-  
+
             <!-- data appartments  -->
             <div class="d-flex flex-column py-2">
               <!-- name appartment  -->
-              <h5 class="fs-2">La Ventana</h5>
+              <h5 class="fs-2">{{ apartment.title }}</h5>
               <!-- address appartment  -->
-              <span class="pb-2">via dei Fori Imperiali, 1</span>
+              <span class="pb-2">{{ apartment.address }}</span>
               <!-- price -->
               <div class="pb-2">
-                <span>330</span>
+                <span>{{ apartment.price }}</span>
                 <span> € a notte</span>
               </div>
               <!-- mq  -->
               <div class="pb-2">
-                <span>220</span>
+                <span>{{ apartment.mq }}</span>
                 <span> mq</span>
               </div>
             </div>
           </div>
           <!-- end card container  -->
-
         </a>
-
       </div>
-      <!-- end card  -->
-  
-    </div>
-  </div>
-  <!-- container da togliere forse  -->
 
+      <!-- end card  -->
+    </div>
+  </li>
 </template>
 
 <style lang="scss" scoped>
+li {
+  list-style: none;
+}
 
+.card-back {
+  height: 370px;
+}
 </style>
