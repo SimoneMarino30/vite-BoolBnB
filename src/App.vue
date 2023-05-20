f76e68e0009cf5cf085f9860be4cf012bf84a63e
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-05-20 17:26:45 +0200

    get user from API

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,10 @@
 <script>
+// STORE
+import { store } from "./data/store.js";
+
+// AXIOS
+import axios from "axios";
+
 // import header
 import AppHeader from "./components/Header/AppHeader.vue";
 
@@ -6,10 +12,112 @@ import AppHeader from "./components/Header/AppHeader.vue";
 import AppFooter from "./components/Footer/AppFooter.vue";
 
 export default {
+
   data() {
-    return {};
+    return {
+      store,
+    };
   },
+
   components: { AppHeader, AppFooter },
+
+  methods: {
+
+    checkLogin() {
+      // Recupero dati URL e salvo in una variabile
+      const urlParams = new URLSearchParams(window.location.search);
+      // console.log(urlParams);
+
+      // Recupero con .get() il valore del primo elemento dell'url
+      const login = urlParams.get('login');
+      // console.log(login);
+
+      // Se login nell'url è true (stabilito dal return nella funzione store di AuthenticatedSessionController)
+      if (login == 'true') {
+
+        // Salvo dati login in sessione
+        this.setSession('login', login);
+        // Salvo dati "auth" in sessione, con metodo .get('parametro') recupero il valore del parametro tra parentesi
+        // console.log(urlParams.get('auth'));
+        this.setSession('auth', urlParams.get('auth'));
+
+        // Dopo aver salvato i dati di sessione li recupero con metodo getSession(), sia per 'login' che per 'auth'
+        const value = this.getSession('login');
+        const id = this.getSession('auth');
+
+        // console.log('CONDIZIONE LOGIN == TRUE - ', value, ' - VALORE AUTH(user_id): ' + id);
+
+        // Ora faccio chiamata axios per recuperare informazioni utente dall'API
+        this.fetchUser()
+
+
+        // Se login nell'url è false (stabilito dal return nella funzione store di AuthenticatedSessionController)
+      } else if (login == 'false') {
+
+        // Cancello dati dalla sesione, sia login che auth
+        this.removeSession('login');
+        this.removeSession('auth');
+        // store.user = null;
+
+        // METODO VARIABILI DI SESSIONE
+        this.removeSession('user');
+      }
+    },
+
+
+    // Utilizzo di "sessionStorage.setItem()" per salvare i dati nella sessione
+    setSession(key, value) {
+      window.sessionStorage.setItem(key, value);
+    },
+
+
+    // Utilizzo di "sessionStorage.getItem()" per recuperare i dati dalla sessione
+    getSession(key) {
+      return window.sessionStorage.getItem(key);
+    },
+
+
+    // Utilizzo di "sessionStorage.removeItem()" per eliminare i dati dalla sessione
+    removeSession(key) {
+      window.sessionStorage.removeItem(key);
+    },
+
+
+    // Funzione per recuperare i dati dell'utente
+    fetchUser() {
+      axios
+        .get(`${this.store.apiUrl}login`, {
+          params: {
+            // Recupero il valore di auth nell'url e lo inserisco come parametro della chiamata API
+            'user_id': this.getSession('auth'),
+          }
+        })
+        .then(response => {
+          console.log(response.data);
+
+          // METODO VARIABILI DI SESSIONE
+          this.setSession('user', response.data);
+          console.log(this.getSession('user'));
+          // this.store.user = this.getSession('user');
+
+
+          // Salvo l'utente
+          this.store.user = response.data;
+        })
+    }
+
+  },
+
+  created() {
+    this.checkLogin();
+
+    // METODO VARIABILI DI SESSIONE
+    // console.log(this.getSession('user'));
+    if (this.getSession('user')) {
+      this.store.user = this.getSession('user');
+      console.log('User: ', this.store.user)
+    }
+  },
 };
 </script>
 
@@ -31,6 +139,4 @@ export default {
 @use "./style/partials/mixins.scss" as *;
 @use "./style/partials/variables.scss" as *;
 @include link_hover;
-
-
 </style>
