57b0d625d5605dca7a800de682c21a19ec68153e
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-05-22 11:03:55 +0200

    improve object user data in file store

--- src/data/store.js
+++ src/data/store.js
@@ -2,5 +2,7 @@ import { reactive } from 'vue'
 
 export const store = reactive({
     apiUrl: "http://127.0.0.1:8000/api/",
-    user: null,
+    user_name: "",
+    user_surname: "",
+    user_email: "",
 });
