718494f9db42c934e93da9c1af51643d323c8ef1
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-05-23 18:47:48 +0200

    update searchbar

--- src/components/Main/SearchBar.vue
+++ src/components/Main/SearchBar.vue
@@ -5,89 +5,80 @@ export default {
   data() {
     return {
       /* search bar */
-      wordSearched: "",
-      searchedApartments: [],
+      // wordSearched: "",
+      // searchedApartments: [],
       address: "",
-      latitudine: null,
-      longitudine: null,
-      name: null,
+      // latitudine: null,
+      // longitudine: null,
+      // name: null,
     };
   },
 
+  emits: ["on-search"],
+
   methods: {
-    searchApartmentsBar() {
-      axios
-        .get("http://127.0.0.1:8000/api/apartments", {
-          params: {
-            adress: this.address,
-            name: this.title,
-          },
-        })
-        .then((response) => {
-          this.searchedApartments = response.data.data;
-          console.log("appartamenti trovati", this.searchedApartments);
 
-          this.searchedApartments.forEach((apartment) => {
-            const lat = this.latitudine;
-            const lon = this.longitudine;
-            const lat1 = apartment.latitude;
-            const lon1 = apartment.longitude;
-          });
-        });
+    search() {
+      this.$emit("on-search", this.address);
     },
 
-    saveCoordinate() {
-      axios
-        .get(
-          "https://api.tomtom.com/search/2/geocode/" +
-            this.address +
-            ".json?key=VTS7KTu4nrOLxN010rCYu364QXAVRCfK",
-          {
-            params: {
-              countrySet: "IT",
-              latitudine: "42.6384261",
-              longitudine: "12.674297",
-              radius: "20000",
-            },
-          }
-        )
-        .then((response) => {
-          this.latitude = response.data.results[0].position.lat;
-          this.longitude = response.data.results[0].position.lon;
+    // searchApartmentsBar() {
+    //   axios
+    //     .get("http://127.0.0.1:8000/api/apartments", {
+    //       params: {
+    //         adress: this.address,
+    //         name: this.title,
+    //       },
+    //     })
+    //     .then((response) => {
+    //       this.searchedApartments = response.data.data;
+    //       console.log("appartamenti trovati", this.searchedApartments);
 
-          console.log("coordinate:", response);
-          console.log("latitudine:", this.latitude);
-          console.log("longitudine:", this.longitude);
-        });
-    },
+    //       this.searchedApartments.forEach((apartment) => {
+    //         const lat = this.latitudine;
+    //         const lon = this.longitudine;
+    //         const lat1 = apartment.latitude;
+    //         const lon1 = apartment.longitude;
+    //       });
+    //     });
+    // },
+
+    // saveCoordinate() {
+    //   axios
+    //     .get(
+    //       "https://api.tomtom.com/search/2/geocode/" +
+    //       this.address +
+    //       ".json?key=VTS7KTu4nrOLxN010rCYu364QXAVRCfK",
+    //       {
+    //         params: {
+    //           countrySet: "IT",
+    //           latitudine: "42.6384261",
+    //           longitudine: "12.674297",
+    //           radius: "20000",
+    //         },
+    //       }
+    //     )
+    //     .then((response) => {
+    //       this.latitude = response.data.results[0].position.lat;
+    //       this.longitude = response.data.results[0].position.lon;
+
+    //       console.log("coordinate:", response);
+    //       console.log("latitudine:", this.latitude);
+    //       console.log("longitudine:", this.longitude);
+    //     });
+    // },
   },
-
-  computed: {},
 };
 </script>
 
 <template>
   <!-- search bar -->
-  <div
-    class="search-bar-container my-3 d-flex align-items-end justify-content-end"
-  >
+  <div class="search-bar-container my-3 d-flex align-items-end justify-content-end">
     <div class="search-bar" id="searchBarContainer">
-      <form
-        class="d-flex"
-        role="search"
-        @submit.prevent="searchApartmentsBar()"
-      >
-        <input
-          class="form-control"
-          type="search"
-          :placeholder="placeholder"
-          aria-label="Search"
-          v-model="address"
-          @input="saveCoordinate"
-          id="address"
-          name="address"
-        />
-        <button class="btn btn-primary mx-2" type="submit">
+      <form class="d-flex" role="search" @submit.prevent="search">
+        <input class="form-control" type="search" :placeholder="placeholder" aria-label="Search" v-model="address"
+          id="address" name="address" />
+        <button class="btn btn-primary mx-2" type="submit" @click="search()">
           <font-awesome-icon icon="fa-solid fa-magnifying-glass" />
         </button>
       </form>
@@ -104,6 +95,7 @@ export default {
 
 .search-bar-container {
   width: 100%;
+
   .search-bar {
     //width: 70%;
   }
