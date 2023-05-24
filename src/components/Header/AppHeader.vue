b186c6b696f2c7dbe8dd557b8297600e2f80fb53
GiuliaGlave <giulia.glave@gmail.com>
2023-05-24 12:18:02 +0200

    Hover sul tasto logout - FRONTEND

--- src/components/Header/AppHeader.vue
+++ src/components/Header/AppHeader.vue
@@ -27,31 +27,29 @@ export default {
     },
 
     logout() {
-
-
       window.history.pushState({}, document.title, "/" + "?login=false");
 
       const urlParams = new URLSearchParams(window.location.search);
-      let login = urlParams.get('login');
+      let login = urlParams.get("login");
 
       console.log(login);
 
-      login == 'false';
+      login == "false";
 
       console.log(login);
 
-      this.removeSession('login');
-      this.removeSession('auth');
+      this.removeSession("login");
+      this.removeSession("auth");
 
-      this.removeSession('name');
-      this.removeSession('surname');
-      this.removeSession('email');
+      this.removeSession("name");
+      this.removeSession("surname");
+      this.removeSession("email");
 
-      this.store.user_name = '';
-      this.store.user_surname = '';
-      this.store.user_email = '';
+      this.store.user_name = "";
+      this.store.user_surname = "";
+      this.store.user_email = "";
     },
-  }
+  },
 };
 </script>
 
@@ -60,62 +58,127 @@ export default {
     <div class="container p-0">
       <nav class="navbar navbar-expand-lg">
         <div class="container-fluid text-center">
-          <a class="navbar-brand" href="/">
-            <img src="../../assets/images/logo-prova.png" alt="Logo" class="d-inline-block align-text-top logo" />
+          <a
+            class="navbar-brand"
+            href="/"
+          >
+            <img
+              src="../../assets/images/logo-prova.png"
+              alt="Logo"
+              class="d-inline-block align-text-top logo"
+            />
           </a>
-          <button class="navbar-toggler color" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll"
-            aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+          <button
+            class="navbar-toggler color"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarScroll"
+            aria-controls="navbarScroll"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span class="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse justify-content-between" id="navbarScroll">
+          <div
+            class="collapse navbar-collapse justify-content-between"
+            id="navbarScroll"
+          >
             <ul class="navbar-nav my-2 my-lg-0">
               <li class="nav-item link-space">
-                <a class="nav-link" aria-current="page" href="/">Home</a>
+                <a
+                  class="nav-link"
+                  aria-current="page"
+                  href="/"
+                  >Home</a
+                >
               </li>
               <li class="nav-item link-space">
-                <router-link class="nav-link" :to="{ name: 'AllApartments' }">
+                <router-link
+                  class="nav-link"
+                  :to="{ name: 'AllApartments' }"
+                >
                   Tutte le Case
                 </router-link>
               </li>
             </ul>
             <!-- buttons -->
-            <div v-if="store.user_email === ''" class="d-flex align-items-center text-center justify-content-center">
+            <div
+              v-if="store.user_email === ''"
+              class="d-flex align-items-center text-center justify-content-center"
+            >
               <a href="http://127.0.0.1:8000/login">
                 <button class="nav-btn">Login</button>
               </a>
               <span class="fs-3">|</span>
 
-              <a href="http://127.0.0.1:8000/register"><button class="nav-btn">Registrati</button>
+              <a href="http://127.0.0.1:8000/register"
+                ><button class="nav-btn">Registrati</button>
               </a>
               <!-- Dropdown LINGUA -->
               <div class="dropdown">
-                <a href="#" class="nav-btn ps-3 dropdown-toggle-no-caret" role="button" id="languageDropdown"
-                  data-bs-toggle="dropdown" aria-expanded="false">
+                <a
+                  href="#"
+                  class="nav-btn ps-3 dropdown-toggle-no-caret"
+                  role="button"
+                  id="languageDropdown"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
                   <font-awesome-icon icon="fa-solid fa-globe" />
                 </a>
-                <ul class="dropdown-menu" aria-labelledby="languageDropdown">
+                <ul
+                  class="dropdown-menu"
+                  aria-labelledby="languageDropdown"
+                >
                   <li>
-                    <a class="dropdown-item" href="#">Italiano</a>
+                    <a
+                      class="dropdown-item"
+                      href="#"
+                      >Italiano</a
+                    >
                   </li>
                   <li>
-                    <a class="dropdown-item" href="#">English</a>
+                    <a
+                      class="dropdown-item"
+                      href="#"
+                      >English</a
+                    >
                   </li>
                 </ul>
               </div>
             </div>
 
-            <div v-else class="d-flex align-items-center text-center justify-content-center">
+            <div
+              v-else
+              class="d-flex align-items-center text-center justify-content-center"
+            >
               <div class="dropdown">
-                <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown"
-                  aria-expanded="false">
+                <button
+                  class="btn btn-secondary dropdown-toggle"
+                  type="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
                   {{ store.user_name }}
                 </button>
                 <ul class="dropdown-menu">
                   <li>
-                    <a class="dropdown-item" href="http://127.0.0.1:8000/dashboard" target="_blank">Area personale</a>
+                    <a
+                      class="dropdown-item"
+                      href="http://127.0.0.1:8000/dashboard"
+                      target="_blank"
+                      >Area personale</a
+                    >
                   </li>
                   <li>
-                    <button @click="logout()" id="logout_button" type="button" class="dropdown-item">Logout</button>
+                    <button
+                      @click="logout()"
+                      id="logout_button"
+                      type="button"
+                      class="dropdown-item"
+                    >
+                      Logout
+                    </button>
                   </li>
                 </ul>
               </div>
@@ -136,10 +199,7 @@ header {
   @include link_hover;
   color: $light_color;
   background-color: rgb(243 228 212);
-  background-color: rgba(34,
-      32,
-      38,
-      0.7);
+  background-color: rgba(34, 32, 38, 0.7);
   /* Colore di sfondo con trasparenza */
 
   .link-space {
@@ -179,6 +239,10 @@ header {
 
     #logout_button {
       color: inherit;
+      &:hover {
+        color: $dark_accent_color;
+        background-color: inherit;
+      }
     }
   }
 }
