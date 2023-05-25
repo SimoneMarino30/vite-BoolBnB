3d12beb982c4d3e7c4b660da5a4fa1c7ed8f4d69
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-05-26 00:13:18 +0200

    improve AllApartments with radius function

--- src/components/pages/AllApartments.vue
+++ src/components/pages/AllApartments.vue
@@ -7,7 +7,6 @@ import { store } from "../../data/store.js";
 
 // COMPONENTS
 import AppList from "../Main/AppList.vue";
-import SearchBar from "../Main/SearchBar.vue";
 import FilterSection from "../Main/FilterSection.vue";
 import Loader from "../pages/Loader.vue";
 
@@ -20,55 +19,104 @@ export default {
       apartments: {
         list: [],
       },
-
+      address: "",
+      lat_a: "",
+      lon_a: "",
       filteredApartments: [],
       resetApartments: [],
+      showAll: true, // Variabile che serve nel template come parametro della condizione per la prop di AppList
     };
   },
 
-  components: { AppList, SearchBar, FilterSection, Loader },
+  components: { AppList, FilterSection, Loader },
 
   // emits: ["changePage"],
 
   methods: {
+
+    // Recupero tutti gli appartamenti
     fetchApartments() {
       this.isLoading = true; // Imposta isLoading su true prima della chiamata
 
       axios.get("http://127.0.0.1:8000/api/apartments").then((response) => {
-        this.apartments.list = response.data;
-        this.filteredApartments = response.data.links;
-        this.resetApartments = response.data.links;
-        this.filterApartments(this.apartments.list);
+        this.apartments.list = response.data.data;
+        // console.log(this.apartments.list);
+
         this.isLoading = false; // Imposta isLoading su false dopo la chiamata
       });
     },
 
+    // Uso tomtom per recuperare coordinate stringa nella searchbar
+    fetchCoordinates() {
+      this.lat_a = "";
+      this.lon_a = "";
+      axios
+        .get(
+          `https://api.tomtom.com/search/2/geocode/${this.address}.json?key=VTS7KTu4nrOLxN010rCYu364QXAVRCfK&countrySet=IT`
+        )
+        .then((response) => {
+          //stampo le coordinate del primo appartamento della mia ricerca, ovvero IL PUNTO GEOGRAFICO DI RIFERIMENTO
+          this.lat_a = response.data.results[0].position.lat;
+          this.lon_a = response.data.results[0].position.lon;
+          // console.log("Coord di riferimento", this.lat_a, this.lon_a);
+
+          this.calculateDistance();
+        });
+    },
+
+    // Setta array appartamento passato nel parametro uguale all'array appartamenti filtrati
     filterApartments(apartments) {
       this.filteredApartments = apartments;
     },
 
-    resetFilters() {
-      this.fetchApartments();
+    // Calcola il raggio e aggiunge appartamenti all'array appartamenti filtrati se la distanza è minore o uguale al range nel parametro
+    calculateDistance(range = 20) {
+      this.filteredApartments = [];
+      // Variabile per non stampare tutti gli appartamenti ma solo quelli filtrati per raggio
+      this.showAll = false;
+      for (let i = 0; i < this.apartments.list.length; i++) {
+        const lat_a = this.lat_a; // Latitudine del punto 1
+        const lon_a = this.lon_a; // Longitudine del punto 1
+
+        //console.log(this.apartments.list[i]);
+
+        const lat_b = this.apartments.list[i].latitude; // Latitudine del punto 2
+        // console.log("Latitudine dell'appartamento " + lat_b);
+        const lon_b = this.apartments.list[i].longitude; // Longitudine del punto 2
+        // console.log("Longitudine dell'appartamento " + lon_b);
+
+        const earthRadius = 6371; // Raggio medio della Terra in chilometri
+
+        const dLat = this.toRadians(lat_b - lat_a);
+        const dLon = this.toRadians(lon_b - lon_a);
+
+        const a =
+          Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+          Math.cos(this.toRadians(lat_a)) *
+          Math.cos(this.toRadians(lat_b)) *
+          Math.sin(dLon / 2) *
+          Math.sin(dLon / 2);
+
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        const distance = earthRadius * c;
+        // console.log(`Distanza: ${distance} km`);
+        if (distance <= range) {
+          this.filteredApartments.push(this.apartments.list[i]);
+        }
+        // console.log("ApP filtrati " + this.searchedApartments);
+        // console.log("i " + i);
+        // console.log("range " + range);
+      }
     },
 
-    // Funzione per cercare appartamento per indirizzo passato dall'emit della SearchBar
-    fetchApartmentsByAddress() {
-      this.isLoading = true;
-      this.apartments.list = [];
-      this.filteredApartments = [];
-      // this.apartments.pages = [];
-      axios
-        .get(`${store.apiUrl}apartments?address=${address.value}`)
-        .then((response) => {
-          console.log(response);
-          this.apartments.list = response.data.data;
-          this.filteredApartments = response.data;
+    //converte gradi in radianti (serve a calculateDistance())
+    toRadians(degrees) {
+      return (degrees * Math.PI) / 180;
+    },
 
-          this.filterApartments(this.apartments.list);
-        })
-        .finally(() => {
-          this.isLoading = false;
-        });
+    // Ritorna l'array chiamato sul created
+    resetFilters() {
+      this.fetchApartments();
     },
   },
 
@@ -81,38 +129,56 @@ export default {
 <template>
   <div class="page-container margin-fix">
     <div class="filter-container d-flex">
-      <FilterSection
-        @filterApartments="filterApartments"
-        :allApartments="filteredApartments"
-        @resetFilters="resetFilters"
-      />
+      <FilterSection @filterApartments="filterApartments" :allApartments="filteredApartments"
+        @resetFilters="resetFilters" />
     </div>
 
     <div class="apartments-container min-height flex-column">
-      <SearchBar @on-search="fetchApartmentsByAddress()" />
+      <!-- <SearchBar @on-search="fetchApartmentsByAddress()" /> -->
+      <!-- * SEARCHBAR -->
+      <div class="search-bar-container my-3 d-flex align-items-end justify-content-end">
+        <div class="search-bar" id="searchBarContainer">
+          <form class="d-flex" role="search" @submit.prevent="fetchCoordinates()">
+            <input class="form-control" type="search" :placeholder="placeholder" aria-label="Search" v-model="address"
+              id="address" name="address" />
+            <button class="btn btn-primary mx-2" type="submit">
+              <font-awesome-icon icon="fa-solid fa-magnifying-glass" />
+            </button>
+          </form>
+        </div>
+      </div>
+
       <Loader v-if="isLoading" />
       <!-- Aggiungi il componente Loader quando isLoading è true -->
 
-      <!-- * Vecchio codice -->
-      <AppList
-        v-else-if="filteredApartments.length > 0"
-        :apartments="filteredApartments"
-      />
-      <div
-        v-else
-        class="text-muted text-center"
-      >
+      <AppList v-else-if="showAll" :apartments="apartments.list" />
+      <AppList v-else-if="!showAll" :apartments="filteredApartments" />
+      <div v-else class="text-muted text-center">
         <h2>Nessun appartamento trovato</h2>
         <h3>Prova Modificando i termini di ricerca</h3>
       </div>
-
-      <!-- * Nuovo codice -->
-      <!-- <AppList :apartments="apartments.list" :pages="apartments.pages" @changePage="fetchApartments" /> -->
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+@use "../../style/partials/variables.scss" as *;
+@use "../../style/partials/mixins.scss" as *;
+@use "../../style/general.scss" as *;
+
+@include btn_hover();
+
+// SEARCHBAR
+.search-bar-container {
+  width: 100%;
+
+  .search-bar {
+    width: 70%;
+    margin: 2rem auto;
+  }
+}
+
+// LIST
 .margin-fix {
   margin-top: 106px;
 }
