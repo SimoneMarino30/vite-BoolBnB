d3cc7882428267f7cc7992049be62ff9e22e78b3
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-05-26 15:10:46 +0200

    start improve style

--- src/components/Footer/AppFooter.vue
+++ src/components/Footer/AppFooter.vue
@@ -19,169 +19,66 @@
           </div>
           <!-- Grid column -->
 
-          <!-- Grid column -->
-          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- STACK -->
+          <div id="stack_list" class="col-md-5 col-lg-5 col-xl-4 mx-auto mb-4">
             <!-- Links -->
-            <h6 class="text-uppercase fw-bold mb-4">Prodotti Usati</h6>
+            <h6 class="text-uppercase text-center fw-bold mb-4">Stack</h6>
 
-            <div class="pb-3">
-              <a
-                href="https://getbootstrap.com/"
-                target="_blank"
-                class=""
-                >Bootstrap</a
-              >
-            </div>
+            <div class="row text-center">
+              <div class="col-4 mb-4 mb-md-0">
+                <a href="https://getbootstrap.com/" target="_blank">
+                  <font-awesome-icon :icon="['fab', 'bootstrap']" />
+                </a>
+              </div>
 
-            <div class="pb-3">
-              <a
-                href="https://vuejs.org/"
-                target="_blank"
-                class=""
-                >Vue</a
-              >
-            </div>
+              <div class="col-4 mb-4 mb-md-0">
+                <a href="https://vuejs.org/" target="_blank">
+                  <font-awesome-icon :icon="['fab', 'vuejs']" />
+                </a>
+              </div>
 
-            <div class="pb-3">
-              <a
-                href="https://vitejs.dev/"
-                target="_blank"
-                class=""
-                >Vite</a
-              >
-            </div>
+              <!-- <div class="col-4 mb-4 mb-md-0">
+              <a href="https://vitejs.dev/" target="_blank" class="">Vite</a>
+            </div> -->
 
-            <div class="pb-3">
-              <a
-                href="https://laravel.com/"
-                target="_blank"
-                class=""
-                >Laravel</a
-              >
+              <div class="col-4 mb-4 mb-md-0">
+                <a href="https://laravel.com/" target="_blank">
+                  <font-awesome-icon :icon="['fab', 'laravel']" />
+                </a>
+              </div>
             </div>
           </div>
-          <!-- Grid column -->
 
           <!-- Grid column -->
           <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
             <!-- Links -->
             <h6 class="text-uppercase fw-bold mb-4">
-              Developers: <Span class=""></Span>
+              Developers <Span class=""></Span>
             </h6>
 
             <div class="pb-3">
-              <a
-                href="https://github.com/SimoneMarino30"
-                target="_blank"
-                class=""
-                >Simone Marino</a
-              >
+              <a href="https://github.com/SimoneMarino30" target="_blank" class="">Simone Marino</a>
             </div>
 
             <div class="pb-3">
-              <a
-                href="https://github.com/GiuliaGlave"
-                target="_blank"
-                class=""
-                >Giulia Glave</a
-              >
+              <a href="https://github.com/GiuliaGlave" target="_blank" class="">Giulia Glave</a>
             </div>
 
             <div class="pb-3">
-              <a
-                href="https://github.com/Gianmarco-Leone"
-                target="_blank"
-                class=""
-                >Gianmarco Leone</a
-              >
+              <a href="https://github.com/Gianmarco-Leone" target="_blank" class="">Gianmarco Leone</a>
             </div>
 
             <div class="pb-3">
-              <a
-                href="https://github.com/MichelepioBombai"
-                target="_blank"
-                class=""
-                >Michele Pio Bombai</a
-              >
+              <a href="https://github.com/MichelepioBombai" target="_blank" class="">Michele Pio Bombai</a>
             </div>
 
             <div class="pb-3">
-              <a
-                href="https://github.com/CarloColletti?tab=repositories"
-                target="_blank"
-                class=""
-                >Carlo Colletti</a
-              >
+              <a href="https://github.com/CarloColletti?tab=repositories" target="_blank" class="">Carlo Colletti</a>
             </div>
           </div>
           <!-- Grid column -->
 
-          <!-- Grid column -->
-          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-            <!-- Links -->
-            <h6 class="text-uppercase fw-bold mb-4">SOCIAL</h6>
-
-            <div class="pb-3">
-              <a
-                href="#!"
-                class=""
-              >
-                <span class="px-1">
-                  <font-awesome-icon icon="fa-brands fa-facebook" />
-                </span>
-                Facebook
-              </a>
-            </div>
-
-            <div class="pb-3">
-              <a
-                href="#!"
-                class=""
-              >
-                <span class="px-1">
-                  <font-awesome-icon icon="fa-brands fa-instagram" />
-                </span>
-                Instagram
-              </a>
-            </div>
-
-            <div class="pb-3">
-              <a
-                href="#!"
-                class=""
-              >
-                <span class="px-1"
-                  ><font-awesome-icon icon="fa-brands fa-twitter"
-                /></span>
-                Twitter
-              </a>
-            </div>
-
-            <div class="pb-3">
-              <a
-                href="#!"
-                class=""
-              >
-                <span class="px-1">
-                  <font-awesome-icon icon="fa-brands fa-google" />
-                </span>
-                Google
-              </a>
-            </div>
-
-            <div class="pb-3">
-              <a
-                href="#!"
-                class=""
-              >
-                <span class="px-1"
-                  ><font-awesome-icon icon="fa-brands fa-github"
-                /></span>
-                Git Hub
-              </a>
-            </div>
-          </div>
-          <!-- Grid column -->
+          <!--  -->
         </div>
         <!-- Grid row -->
       </div>
@@ -200,7 +97,14 @@
 @use "../../style/general.scss" as *;
 @use "../../style/partials/variables.scss" as *;
 @use "../../style/partials/mixins.scss" as *;
+
 footer {
   @include link_hover;
+
+  #stack_list {
+    a {
+      font-size: 2rem;
+    }
+  }
 }
 </style>
