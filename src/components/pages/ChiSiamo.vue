f6fad970f9ee9b1749292966782b2c6245fb3c43
GiuliaGlave <giulia.glave@gmail.com>
2023-05-28 14:21:16 +0200

    chi siamo giulia

--- src/components/pages/ChiSiamo.vue
+++ src/components/pages/ChiSiamo.vue
@@ -19,7 +19,8 @@ export default {
             <div class="img1">
               <img
                 src="https://media.licdn.com/dms/image/D4E03AQFEiyYs1qZAFA/profile-displayphoto-shrink_800_800/0/1683387668985?e=1690416000&v=beta&t=_NdOc9IXPHt2N1Hjhfe9H78HuWfowROZCqUcQmdmaeY"
-                alt="" />
+                alt=""
+              />
             </div>
             <div class="card-body">
               <h4 class="card-title text-center">Michele Pio Bombai</h4>
@@ -34,10 +35,19 @@ export default {
             </div>
             <div class="card-footer">
               <div class="text-center">
-                <a href="https://www.linkedin.com/in/michele-pio-bombai-071348275" target="_blank">
-                  <font-awesome-icon :icon="['fab', 'linkedin']" class="me-3" />
+                <a
+                  href="https://www.linkedin.com/in/michele-pio-bombai-071348275"
+                  target="_blank"
+                >
+                  <font-awesome-icon
+                    :icon="['fab', 'linkedin']"
+                    class="me-3"
+                  />
                 </a>
-                <a href="https://github.com/MichelePioBombai" target="_blank">
+                <a
+                  href="https://github.com/MichelePioBombai"
+                  target="_blank"
+                >
                   <font-awesome-icon :icon="['fab', 'github']" />
                 </a>
               </div>
@@ -55,10 +65,19 @@ export default {
             </div>
             <div class="card-footer">
               <div class="text-center">
-                <a href="https://www.linkedin.com/in/michele-pio-bombai-071348275" target="_blank">
-                  <font-awesome-icon :icon="['fab', 'linkedin']" class="me-3" />
+                <a
+                  href="https://www.linkedin.com/in/michele-pio-bombai-071348275"
+                  target="_blank"
+                >
+                  <font-awesome-icon
+                    :icon="['fab', 'linkedin']"
+                    class="me-3"
+                  />
                 </a>
-                <a href="https://github.com/CarloColletti?tab=repositories" target="_blank">
+                <a
+                  href="https://github.com/CarloColletti?tab=repositories"
+                  target="_blank"
+                >
                   <font-awesome-icon :icon="['fab', 'github']" />
                 </a>
               </div>
@@ -69,24 +88,37 @@ export default {
           <div class="card h-100">
             <div class="img1">
               <img
-                src="https://media.licdn.com/dms/image/D4D03AQGIsTT9eby63g/profile-displayphoto-shrink_400_400/0/1683102580430?e=1690416000&v=beta&t=2WS6XU_89qWxvim42gtRnS06yJzY3fDjC7d5NI8IySI" />
+                src="https://media.licdn.com/dms/image/D4D03AQGIsTT9eby63g/profile-displayphoto-shrink_400_400/0/1683102580430?e=1690416000&v=beta&t=2WS6XU_89qWxvim42gtRnS06yJzY3fDjC7d5NI8IySI"
+              />
             </div>
             <div class="card-body">
               <h4 class="card-title text-center">Simone Marino</h4>
               <p class="card-text text-center">
-                Sono un appassionato di tecnologia, in particolare dell'informatica e dello sviluppo web,
-                utilizzo molto del mio tempo libero per studiare e capire le nuove tecnologie proposte e rimanere
-                costantemente aggiornato sulle ultime tendenze del settore. <br>
-                Inoltre, sono molto appassionato di viaggi e, quando non scrivo linee dicodice, amo passare il mio tempo
-                scoprendo posti, cibi, storia e cultura dei posti che visito.
+                Sono un appassionato di tecnologia, in particolare
+                dell'informatica e dello sviluppo web, utilizzo molto del mio
+                tempo libero per studiare e capire le nuove tecnologie proposte
+                e rimanere costantemente aggiornato sulle ultime tendenze del
+                settore. <br />
+                Inoltre, sono molto appassionato di viaggi e, quando non scrivo
+                linee dicodice, amo passare il mio tempo scoprendo posti, cibi,
+                storia e cultura dei posti che visito.
               </p>
             </div>
             <div class="card-footer">
               <div class="text-center">
-                <a href="https://www.linkedin.com/in/michele-pio-bombai-071348275" target="_blank">
-                  <font-awesome-icon :icon="['fab', 'linkedin']" class="me-3" />
+                <a
+                  href="https://www.linkedin.com/in/simone-marino-8930/"
+                  target="_blank"
+                >
+                  <font-awesome-icon
+                    :icon="['fab', 'linkedin']"
+                    class="me-3"
+                  />
                 </a>
-                <a href="https://github.com/SimoneMarino30" target="_blank">
+                <a
+                  href="https://github.com/SimoneMarino30"
+                  target="_blank"
+                >
                   <font-awesome-icon :icon="['fab', 'github']" />
                 </a>
               </div>
@@ -101,18 +133,34 @@ export default {
         <div class="col-lg-3 col-md-6 col-sm-12">
           <div class="card h-100">
             <div class="img1">
-              <img src="" />
+              <img src="../../assets/images/giulia.jpg" />
             </div>
             <div class="card-body">
               <h4 class="card-title text-center">Giulia Glave</h4>
-              <p class="card-text text-center"></p>
+              <p class="card-text text-center">
+                Appassionata di tecnologia, con un grande interesse verso lo
+                sviluppo web, una solida formazione in lingue e un'esperienza
+                pluriennale nel settore turistico. Sono sempre alla ricerca di
+                nuove sfide e di migliorare le mie abilità. La carriera nel web
+                development mi ha permesso di unire le mie passioni al desiderio
+                di crescere e alla creazione di soluzioni innovative.
+              </p>
             </div>
             <div class="card-footer">
               <div class="text-center">
-                <a href="https://www.linkedin.com/in/michele-pio-bombai-071348275" target="_blank">
-                  <font-awesome-icon :icon="['fab', 'linkedin']" class="me-3" />
+                <a
+                  href="https://www.linkedin.com/in/giulia-glave/"
+                  target="_blank"
+                >
+                  <font-awesome-icon
+                    :icon="['fab', 'linkedin']"
+                    class="me-3"
+                  />
                 </a>
-                <a href="https://github.com/GiuliaGlave" target="_blank">
+                <a
+                  href="https://github.com/GiuliaGlave"
+                  target="_blank"
+                >
                   <font-awesome-icon :icon="['fab', 'github']" />
                 </a>
               </div>
@@ -127,17 +175,28 @@ export default {
             <div class="card-body">
               <h4 class="card-title text-center">Gianmarco Leone</h4>
               <p class="card-text text-center">
-                Sono un appassionato di tecnologia e programmazione fin da bambino. L'emozione di creare codice e
-                sperimentare nuove soluzioni è il mio carburante. Sono affascinato dall'evoluzione digitale e non vedo
-                l'ora di continuare a imparare e crescere in questo affascinante mondo.
+                Sono un appassionato di tecnologia e programmazione fin da
+                bambino. L'emozione di creare codice e sperimentare nuove
+                soluzioni è il mio carburante. Sono affascinato dall'evoluzione
+                digitale e non vedo l'ora di continuare a imparare e crescere in
+                questo affascinante mondo.
               </p>
             </div>
             <div class="card-footer">
               <div class="text-center">
-                <a href="https://www.linkedin.com/in/gianmarco-leone-843745212/" target="_blank">
-                  <font-awesome-icon :icon="['fab', 'linkedin']" class="me-3" />
+                <a
+                  href="https://www.linkedin.com/in/gianmarco-leone-843745212/"
+                  target="_blank"
+                >
+                  <font-awesome-icon
+                    :icon="['fab', 'linkedin']"
+                    class="me-3"
+                  />
                 </a>
-                <a href="https://github.com/Gianmarco-Leone" target="_blank">
+                <a
+                  href="https://github.com/Gianmarco-Leone"
+                  target="_blank"
+                >
                   <font-awesome-icon :icon="['fab', 'github']" />
                 </a>
               </div>
